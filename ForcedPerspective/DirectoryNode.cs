40115d82fa553d9c3bf3f5bbcecce7349d740f0b
thealexhoar <Lacto123>
2015-03-24 14:59:12 -0700

    Inital commit. Generates tree based on filesystem.

---
+++ ForcedPerspective/DirectoryNode.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ForcedPerspective {
+    class DirectoryNode {
+
+        public List<DirectoryNode> subDirectories;
+        public List<FileInfo> files;
+
+        public string path;
+        public long size = 0;
+        public bool hasSubDirectories = false;
+        public bool hasFiles = false;
+        public int currentDepth, maxDepth;
+
+        public DirectoryNode(String path, int depth, int maxdepth) {
+            this.path = path;
+            currentDepth = depth;
+            maxDepth = maxdepth;
+            Console.WriteLine(path);
+            subDirectories = new List<DirectoryNode>();
+            files = new List<FileInfo>();
+            string[] sub = Directory.GetDirectories(this.path);
+            if (currentDepth + 1 < maxDepth) {
+                if (sub.Length > 0) {
+                    hasSubDirectories = true;
+                    DirectoryNode d;
+                    for (int i = 0; i < sub.Length; i++) {
+                        try {
+                            d = new DirectoryNode(sub[i], currentDepth + 1, maxdepth);
+                            size += d.size;
+                            subDirectories.Add(d);
+                        }
+                        catch { }
+                    }
+                }
+            }
+            else {
+                for (int i = 0; i < sub.Length; i++) {
+                    //try {
+                    size += getDirectorySize(sub[i]);
+                    //} catch { }
+                }
+            }
+            string[] containedFiles = Directory.GetFiles(this.path);
+            if (containedFiles.Length > 0) {
+                hasFiles = true;
+                FileInfo f;
+                for (int i = 0; i < containedFiles.Length; i++) {
+                    try {
+                        f = new FileInfo(containedFiles[i]);
+                        size += f.Length;
+                        files.Add(f);
+                    }
+                    catch (Exception e) { }
+                }
+            }
+        }
+
+        public long getBytes() {
+            return size;
+        }
+
+        public float getKiloBytes() {
+            return (size / 1024.0f);
+        }
+
+        public float getMegaBytes() {
+            return (size / 1024.0f / 1024.0f);
+        }
+
+        public float getGigaBytes() {
+            return (size / 1024.0f / 1024.0f / 1024.0f);
+        }
+
+        private long getDirectorySize(String location) {
+            DirectoryInfo di = new DirectoryInfo(location);
+            //IEnumerable<FileInfo> ugh = di.EnumerateFiles("*.*", SearchOption.AllDirectories);//.Sum(fi => fi.Length);
+            FileInfo[] s = di.GetFiles("*.*", SearchOption.AllDirectories);
+            return 10;
+        }
+
+    }
+}
---
+++ ForcedPerspective/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForcedPerspective {
+    public partial class Form1 : Form {
+
+        System.Drawing.Bitmap bitmap;
+
+
+        public Form1() {
+            InitializeComponent();
+            bitmap = new Bitmap(251, 184);
+            int r,g,b;
+            DirectoryNode d = new DirectoryNode(@"C:\", 0, 100);
+            Console.WriteLine(d.getGigaBytes());
+
+
+            for (int x = 0; x < 251; x++) {
+                for (int y = 0; y < 184; y++) {
+                    
+                    r = 255 * (x / 251);
+                    g = 255 * ((x + y) / (251 + 184));
+                    b = 255 * (y / 184);
+                    bitmap.SetPixel(x, y, Color.FromArgb(x,0,y));
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+            pictureBox1.Image = bitmap;
+        }
+    }
+}
